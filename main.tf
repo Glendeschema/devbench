ab5be83998b5654e31ca167528d82d970d6bb956
GlenTsime <lehlohonologlen@gmail.com>
2020-09-22 16:37:36 +0200

    added extension for Nginx custom script extension

--- main.tf
+++ main.tf
@@ -89,7 +89,19 @@ resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
   admin_username                  = "adminuser"
   disable_password_authentication = false
 
-
+  extension {
+    name                 = "install-nginx"
+    publisher            = "Microsoft.Azure.Extensions"
+    type                 = "CustomScript"
+    type_handler_version = "2.0"
+ 
+    settings = <<SETTINGS
+    {
+    "fileUris": ["https://schematerraformstate.blob.core.windows.net/code/apache2.sh"],
+    "commandToExecute": "bash apache2.sh"
+    }
+SETTINGS
+  }
   admin_password = "Munch13w@k@@2020"
 
   source_image_reference {
